3d0c7563b6a37099b71af2398a8ba0013ce2caca
Yaser Bashir <yaser.bashir@gmx.de>
2016-09-17 11:47:14 +0200

    Finish layout and routes

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get usignup_path
+    get signup_path
     assert_response :success
   end
 
